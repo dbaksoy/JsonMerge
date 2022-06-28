c6605f85c113b19ca3c0283c6d0f923a6a98c59f
binarynotfound <dbaranaksoy@gmail.com>
2022-06-28 14:44:18 +0300

    Add parseJson function to parse data

--- include/bouncmpe/jsonmerge.hpp
+++ include/bouncmpe/jsonmerge.hpp
@@ -10,6 +10,9 @@ auto jsonmerge(
   -> boost::json::value;
 
 // Extend the interface to handle by JSON files from the filesystem  
+
+// Parse two JSON files and return JSON value
+auto parseJson(std::string jsonFileName) -> boost::json::value;
 // Check data folder for some example files - you can use std::filesystem
 
 }  // namespace bouncmpe
--- src/jsonmerge.cpp
+++ src/jsonmerge.cpp
@@ -19,4 +19,18 @@ auto jsonmerge(
 
 // Implement the rest of the functionality and any other helpers if necessary
 
+// Parse two JSON files
+
+auto parseJson(std::string jsonFileName) -> boost::json::value
+  {
+    // Read from file
+    std::ifstream t(jsonFileName);
+    // Create buffer and holds the file inside it
+    std::stringstream buffer;
+    buffer << t.rdbuf();
+    // Parse the data
+    std::string stringjson = buffer.str();
+    auto parsed_data = boost::json::parse(stringjson);
+    return parsed_data;
+  }
 }  // namespace bouncmpe
